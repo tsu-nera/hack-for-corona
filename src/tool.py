ab0212f38f4f3305fd6dbd743b3f1c12e3592018
tsu-nera <fox10225fox@gmail.com>
2020-03-22 16:42:35 +0900

    feat: add エリア別感染者数

--- src/tool.py
+++ src/tool.py
@@ -5,3 +5,10 @@ import src.constants as const
 
 def read_patients():
     return pd.read_csv(const.MASTER_FILE_PATH)
+
+
+def cut_ku(x):
+    if "区" in x:
+        return x.split("市")[0] + "市"
+    else:
+        return x
